7827b2e302dc078bc50e43a10aceebe4d699745a
Paramjeet Singh <dapp.programmer@gmail.com>
2022-01-13 00:23:36 +0530

    Completed Navbar component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -13,8 +13,9 @@ const Navbar = () => {
 
   return (
     <nav className="w-full flex md:justify-center justify-between items-center p-4">
-      <div className="md:flex-[0.5] flex-initial justify-center items-center">
+      <div className="flex md:flex-[0.5] flex-initial justify-center items-center">
         <img src={logo} alt="logo" className="h-12 w-12 cursor-pointer" />
+        <span className="text-white text-lg" >eth swap</span>
       </div>
       <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
         {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
