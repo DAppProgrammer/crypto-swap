97e966f4f77c06572af4019a6f9cecb2b3d0a8b7
Paramjeet Singh <dapp.programmer@gmail.com>
2022-02-04 11:53:00 +0530

    deploy contract without truffle  using web3

--- web3/generateABI.js
+++ web3/generateABI.js
@@ -25,5 +25,24 @@ var input = {
 };
 
 const output = JSON.parse(solc.compile(JSON.stringify(input)));
-console.log('ABI: ',output.contracts["storage.sol"]["Storage"].abi);
-console.log('ByteCode: ',output.contracts["storage.sol"]["Storage"].evm.bytecode.object);
+const abi = output.contracts["storage.sol"]["Storage"].abi;
+const byteCode = output.contracts["storage.sol"]["Storage"].evm.bytecode.object;
+
+/* DEPLOY THE CONTRACT */
+(async () => {
+  const accounts = await web3.eth.getAccounts();
+  const account1 = accounts[0];
+  const contract = new web3.eth.Contract(abi);
+
+  contract
+    .deploy({ data: byteCode })
+    .send({ from: account1, gas: 500000 })
+    .on("receipt", (receipt) => {
+      console.log("Contract address: ", receipt.contractAddress);
+    })
+    .then((storageContract) => {
+      storageContract.methods.retrieve().call((err, data) => {
+        console.log("initial val: ", data);
+      });
+    });
+})();
